9bead3a12ae2e4d34abbe79c285bf8098cc5d8f9
northkevin <dev@northkevin.com>
2025-06-29 08:21:16 -0700

    better logos and icons

---
+++ frontend/convert-favicon.cjs
@@ -0,0 +1,15 @@
+const sharp = require("sharp");
+
+const input = "public/favicon.svg";
+const output = "public/favicon-32x32.png";
+
+sharp(input)
+  .resize(32, 32)
+  .png()
+  .toFile(output)
+  .then(() => {
+    console.log("Favicon PNG generated:", output);
+  })
+  .catch((err) => {
+    console.error("Error generating PNG favicon:", err);
+  });
