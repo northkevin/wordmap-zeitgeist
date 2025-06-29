0d146e04441a6543579c8c1b473420b419266291
Kevin North <contact@northkevin.com>
2025-06-29 00:30:55 -0700

    Fix unused React import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import WordMap from './components/WordMap'
 import Header from './components/Header'
